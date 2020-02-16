fffeea87f5ae1afd193993f4e8386dc7d4d65d29
sandy <sandy-encarnacion@live.com>
2020-02-16 14:35:02 -0400

    Revert "Revert "new commit""
    This reverts commit 4fe376317782811c323fe2c7d2f1d07bdd754cde.

--- TablaPeriodica/Startup.cs
+++ TablaPeriodica/Startup.cs
@@ -53,7 +53,7 @@ namespace TablaPeriodica
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Tabla}/{action=Index}/{id?}");
             });
         }
     }
